9b8c8c09424557b2edbc51253aaa97ab2681c8d2
Ajani Timothy <timothyinioluwa@gmail.com>
2024-07-20 08:56:07 +0100

    refactor: refactored code for better readability

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -9,48 +9,41 @@ interface LoginFormData {
 }
 
 const LoginForm = () => {
-  
-    const [showPassword,setShowPassword] =useState(false)
-    const [isLoading, setLoading] = useState(false);
-    const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-    const { register, handleSubmit, formState: { errors } } = useForm<LoginFormData>();
-    
-    const onSubmit: SubmitHandler<LoginFormData> = (_data) => {
-      setLoading(true);
-      setTimeout(() => {
-        setLoading(false);
-        navigate('/dashboard/users');
-      }, 2000);
-    };
+  const { register, handleSubmit, formState: { errors } } = useForm<LoginFormData>();
 
- 
+  const onSubmit: SubmitHandler<LoginFormData> = (_data) => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      navigate('/dashboard/users');
+    }, 2000);
+  };
 
-  const togglePassword = ()=>{
-    setShowPassword(!showPassword)
-  }
-  
+  const togglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
-    <>
-    <form  onSubmit={handleSubmit(onSubmit)}>
-        <div className='login__input-container' >
-            <input type="email" placeholder="Email"  {...register("email", { required: true })} />
-            {errors.email && <span className="login-form__error-message" >Email Address is required</span>}
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="login__input-container">
+        <input type="email" placeholder="Email" {...register("email", { required: true })} />
+        {errors.email && <span className="login__error">Email Address is required</span>}
+      </div>
+      <div className="login__input-container">
+        <div className="login__password">
+          <input type={showPassword ? "text" : "password"} placeholder="Password" {...register("password", { required: true })} />
+          <span className="login__password-toggler" onClick={togglePassword}>{showPassword ? "HIDE" : "SHOW"}</span>
         </div>
-        <div className='login__input-container'>
-            <div className='login-form__input-password' >
-
-            <input type={showPassword ? "text" : "password"} placeholder="Password"  {...register("password", { required: true })} />
-            <span  className="login-form__password-toggler" onClick={togglePassword} >{showPassword ? "HIDE" : "SHOW"}</span>
-            </div>
-            {errors.password && <span className="login-form__error-message" >Password is required</span>}
-        </div>
-        <small >FORGOT PASSWORD ?</small>
-        <button disabled={isLoading} >{isLoading ? "LOADING" : "LOG IN"} {isLoading && <ThreeDotLoader /> }</button>
-       
+        {errors.password && <span className="login__error">Password is required</span>}
+      </div>
+      <small>FORGOT PASSWORD?</small>
+      <button disabled={isLoading}>{isLoading ? "LOADING" : "LOG IN"} {isLoading && <ThreeDotLoader />}</button>
     </form>
-    </>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,34 +1,29 @@
-import '../styles/pages/login.scss'
-import lendsqrLogo from '../assets/images/logo.svg'
-import lendsqrLoginIllustration from '../assets/images/login-illustration.svg'
-import LoginForm from '../components/forms/LoginForm'
+import '../styles/pages/login.scss';
+import lendsqrLogo from '../assets/images/logo.svg';
+import lendsqrLoginIllustration from '../assets/images/login-illustration.svg';
+import LoginForm from '../components/forms/LoginForm';
 
 const LoginPage = () => {
   return (
-    <div className='login' >
+    <div className="login">
       <div className="login__container">
-        <section className='login__left-container' >
-          
-            <img src={lendsqrLogo} className='login__logo' alt="lendsqr-logo" />
-         
-          <div className='login__illustration-container'>
-            <img src={lendsqrLoginIllustration} className='login__illustration' alt="lendsqr-login-illustration" />
+        <section className="login__left">
+          <img src={lendsqrLogo} className="login__logo" alt="lendsqr-logo" />
+          <div className="login__illustration-container">
+            <img src={lendsqrLoginIllustration} className="login__illustration" alt="lendsqr-login-illustration" />
           </div>
         </section>
-        <section className='login__right-container' >
-
-          <img src={lendsqrLogo} alt="logo for small screens" />
-
-          <div className='login__form-container' >
-           <h3 >Welcome!</h3>
-            <p >Enter details to login.</p>
+        <section className="login__right">
+          <img src={lendsqrLogo} className="login__right__logo" alt="logo for small screens" />
+          <div className="login__form">
+            <h3>Welcome!</h3>
+            <p>Enter details to login.</p>
             <LoginForm />
           </div>
         </section>
-        
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
