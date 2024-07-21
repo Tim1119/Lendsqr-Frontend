96f2bfde71dfb27a5f6b47dadfa8cbe5fcd10fa7
Ajani Timothy <timothyinioluwa@gmail.com>
2024-07-21 18:36:23 +0100

    fix: made changes to colors and fonts in navbar,sidelink and sidebar

--- src/components/dashboard/Sidebar.tsx
+++ src/components/dashboard/Sidebar.tsx
@@ -1,53 +1,60 @@
-import { SidebarNavigationData } from '../../utils/SidebarNavigation';
+
+import {SidebarNavigationData} from '../../utils/SidebarNavigation'
 import SidebarLink from './SidebarLink';
 import { useLocation } from 'react-router-dom';
-import '../../styles/components/dashboard/sidebar.scss';
-import lendsqrLogo from '../../assets/images/logo.svg';
-import closeIcon from '../../assets/icons/navbar/close.svg';
+import '../../styles/components/dashboard/sidebar.scss'
+import lendsqrLogo from '../../assets/images/logo.svg'
+import closeIcon from '../../assets/icons/navbar/close.svg'
 
 interface SidebarProps {
-  isOpen: boolean;
-  toggleSidebar: () => void;
-}
+    isOpen: boolean;
+    toggleSidebar: () => void;
+  }
 
-const Sidebar = ({ isOpen, toggleSidebar }: SidebarProps) => {
-  const pathname: string = useLocation().pathname;
+const Sidebar = ({ isOpen, toggleSidebar }:SidebarProps) => {
 
-  const isRouteActive = (route: string) => {
-    if (pathname === '/dashboard' && route === '/dashboard') {
-      return true;
-    }
-    const path = route.split('/')[2];
-    return Boolean(pathname.includes(path));
-  };
+    const pathname:string = useLocation().pathname;
+	
+
+    const isRouteActive = (route:string) => {
+		if (pathname === '/dashboard' && route === '/dashboard') {
+			return true;
+		}
+		const path = route.split('/')[2];
+		return Boolean(pathname.includes(path));
+	};
 
   return (
-    <div className={`sidebar ${isOpen ? 'sidebar--open' : ''}`}>
-      <div className="sidebar__header">
+    <div className={`sidebar ${isOpen ? 'sidebar--toggle' : ''}`}>
+       
+      {/* Sidebar content */}
+      <div className="sidebar__nav-header">
         <div>
-          <img className="sidebar__logo" src={lendsqrLogo} alt="logo" />
+            <img className='sidebar__logo' src={lendsqrLogo} alt="logo"  />
         </div>
-        <div className="sidebar__header__close-icon-container" onClick={toggleSidebar}>
-          <img src={closeIcon} className="sidebar__header__close-icon" alt="close" />
+        <div className='sidebar__close-icon-container' onClick={toggleSidebar} >
+            <img src={closeIcon} className='sidebar__close-icon' alt="close" />
         </div>
       </div>
 
-      {SidebarNavigationData.map((sidebarLinkData, index) => (
-        <div key={index}>
-          <SidebarLink
-            key={index}
-            title={sidebarLinkData.title}
-            icon={sidebarLinkData.icon}
-            link={sidebarLinkData.link}
-            toggleIcon={sidebarLinkData.toggleIcon}
-            id={sidebarLinkData.id}
-            header={sidebarLinkData.header}
-            isActive={isRouteActive(sidebarLinkData.link)}
-          />
+      {SidebarNavigationData.map((sidebarLinkData,index)=>(
+        <div key={index} className='sidebar__main-links' > 
+         <SidebarLink 
+            key={index} 
+                title={sidebarLinkData.title} 
+                icon={sidebarLinkData.icon} 
+                link={sidebarLinkData.link} 
+                toggleIcon={sidebarLinkData.toggleIcon}
+                id={sidebarLinkData.id} 
+                header={sidebarLinkData.header} 
+                isActive={isRouteActive(sidebarLinkData.link)} 
+                
+        />
         </div>
       ))}
+     
     </div>
-  );
-};
+  )
+}
 
-export default Sidebar;
+export default Sidebar
--- src/components/dashboard/SidebarLink.tsx
+++ src/components/dashboard/SidebarLink.tsx
@@ -1,45 +1,41 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
-import '../../styles/components/dashboard/sidebar-link.scss';
+import '../../styles/components/dashboard/sidebar-link.scss'
 
-interface SidebarLinkProps {
-  title: string;
-  icon?: string;
-  link: string;
-  toggleIcon?: string;
-  id: number;
-  header?: boolean;
-  isActive: boolean;
+interface SidebarLinkProps{
+  title:string;
+  icon?:string;
+  link:string;
+  toggleIcon?:string;
+  id:number;
+  header?:boolean;
+  isActive:boolean;
 }
 
-const SidebarLink: React.FC<SidebarLinkProps> = ({ title, icon, link, toggleIcon, id, header, isActive }) => {
+const SidebarLink = ({title,icon,link,toggleIcon,id,header,isActive}:SidebarLinkProps) => {
   return (
-    <>
-      {!header ? (
-        <Link
-          to={link}
-          className={`sidebar-link ${isActive ? 'sidebar-link--active' : ''} ${id === 26 ? 'sidebar-link--logout' : ''} ${id < 3 || id > 25 ? 'sidebar-link--large' : ''} ${id === 26 ? 'sidebar-link--border-top' : ''}`}
-        >
-          {icon && (
-            <span className="sidebar-link__icon-container">
-              <img className="sidebar-link__icon" src={icon} alt={`${title}-icon`} />
-            </span>
-          )}
-          <span className="sidebar-link__title">{title}</span>
-          {toggleIcon && (
-            <span className="sidebar-link__toggle">
-              <img src={toggleIcon} alt="toggle-icon" />
-            </span>
-          )}
-        </Link>
-      ) : (
-        <li className={`sidebar-link ${header ? 'sidebar-link--header' : ''}`}>
-          <span className="sidebar-link__header">{title}</span>
-        </li>
-      )}
-      {id === 26 && <small className="sidebar-link__version">v1.2.0</small>}
-    </>
-  );
-};
+    <> 
+    
+    {!header ? (
 
-export default SidebarLink;
+    <Link to={link} className={`sidebarlink ${isActive && "sidebarlink--active"} ${(id === 1 || id === 2) && '--topmost-links'} ${id === 26 && 'sidebarlink__logout'} ${!isActive && "sidebarlink--hover"} ${id < 3 || id > 25 && "sidebarlink--bigger"} ${id===26 && 'border--top'}`}>
+      {icon &&
+        <span className="sidebarlink__icon-container" >
+         <img className="sidebarlink__icon" src={icon} alt={`${title}-icon`} />
+         </span>
+         }
+        <span className="sidebarlink__title" >{title}</span>
+      {toggleIcon && <span className="sidebarlink__toggle" ><img src={toggleIcon} alt="toggle-icon" /></span>}
+    </Link>
+    ):(
+    <li className={`sidebarlink ${header && 'sidebar--header-small'} `}>
+        <span className={ "sidebarlink__title-header" }>{title}</span>
+    </li>
+
+    )}
+    {id === 26 && <small className="sidebarlink__version" >v1.2.0 </small> }
+   
+    </>
+  )
+}
+
+export default SidebarLink
